d6a36a603f5014310ee64964a2d90c980c54db0b
Fred Westling <fredrik.westling@gmail.com>
2017-10-25 14:52:35 +1100

    Reorganised everything roughly into HTML related and scheduler related; basically, split up UI and behaviour.  This should allow me to implement load/save pretty easily, as well as take the next step into actual scheduling.

--- scheduler/event_definitions.js
+++ scheduler/event_definitions.js
@@ -6,7 +6,6 @@ const TYPE_BREAK = new sessionType("Breaks", 0);
 var UID_counter = 1;
 var start_time_offset = 0; // Set to number of minutes to start if wanted
 
-
 function sessionType(name,priority) {
 	this.name = name;
 	this.priority = priority;
@@ -23,42 +22,15 @@ function EventParameters(name,nTeams,nDays,teamNumbers,teamNames,days) {
 	while (this.days.length < this.nDays) this.days.push("Day " + (this.days.length+1));
 	while (this.teamNumbers.length < this.nTeams) this.teamNumbers.push("" + (this.teamNumbers.length+1)); 
 	while (this.teamNames.length < this.nTeams) this.teamNames.push("Team " + (this.teamNames.length+1)); 
-
-	this.changeTitle = function() {
-	    var safe = this.name;
-	    this.name = prompt("Enter title here", $("#title").get(0).textContent);
-	    if (this.name == null) this.name = safe;
-	    document.getElementById("title").innerHTML = this.name;
-	}
-	this.changeNTeams = function() {
-		this.nTeams = $("#nTeams")[0].value;
-		while (this.teamNumbers.length < this.nTeams) this.teamNumbers.push("" + (this.teamNumbers.length+1)); 
-		while (this.teamNames.length < this.nTeams) this.teamNames.push("Team " + (this.teamNames.length+1)); 
-		while (this.teamNumbers.length > this.nTeams) this.teamNumbers.splice(this.teamNumbers.length,1); 
-		while (this.teamNames.length > this.nTeams) this.teamNames.splice(this.teamNames.length,1); 
-	}
-}
-
-function updateDays() {
-	tournament.nDays = $("#nDays")[0].value;
-	while (tournament.days.length < tournament.nDays) {
-		tournament.days.push("Day "+ (tournament.days.length+1));
-		addBreak("Night "+(tournament.days.length-1),((tournament.days.length-1)*24*60-360),((tournament.days.length-1)*24*60+540));
-	}
-	while (tournament.days.length > tournament.nDays) {
-		tournament.days.splice(tournament.days.length-1,1);
-	}
-	var toDelete = [];
-	for (var i = 0; i < tournament.allSessions.length; i++) {
-		var session = tournament.allSessions[i];
-		if (session.type == TYPE_BREAK && session.end > tournament.nDays*(24*60))
-			toDelete.push(session.uid);
-		while (session.start > tournament.nDays*(24*60)) session.start -= (24*60);
-		while (session.end > tournament.nDays*(24*60)) session.end -= (24*60);
-		session.updateDOM();
-	}
-	for (var i = 0; i < toDelete.length; i++) {
-		deleteParams(toDelete[i]);
+	this.updateDays = function(num_days) {
+		this.nDays = num_days;
+		while (this.days.length < this.nDays) {
+			this.days.push("Day "+ (this.days.length+1));
+			addBreak("Night "+(this.days.length-1),((this.days.length-1)*24*60-360),((this.days.length-1)*24*60+540));
+		}
+		while (this.days.length > this.nDays) {
+			this.days.splice(this.days.length-1,1);
+		}
 	}
 }
 
@@ -97,124 +69,6 @@ function SessionParameters(type,name,start,end,nSims,nLocs,length,buffer,locs) {
 		if (this.type == TYPE_BREAK) this.buffer = 0;
 	}
 	this.locations = locs || [];
-	// Create elements of DOM input form
-	this.doms = new DomCollection();
-	this.docObj = $("<table class=roundtable>");
-	this.doms.locsInput.attr('value',this.nLocs);
-	this.ToForm = function() {
-		var dom = this.docObj;
-		if (this.type != TYPE_BREAK)
-			var x = $("<tr><td><h3></h3></td><td><button class=\"btn\" onclick=\"copyToAll("+this.uid+")\">Copy to all</button></td></tr>");
-		else var x = $("<tr><td colspan=\"2\"><h3></h3></td></tr>");
-		$("h3", x).append(this.doms.title);
-		dom.append(x);
-		var x = $("<tr><td>Start time:</td><td><div></div></td></tr>");
-		$("div", x).append(this.doms.startDateInput);
-		$("div", x).append(this.doms.startTimeInput);
-		dom.append(x);
-		var x = $("<tr><td>Must be done by:</td><td><div></div></td></tr>");
-		$("div", x).append(this.doms.endDateInput);
-		$("div", x).append(this.doms.endTimeInput);
-		dom.append(x);
-		if (this.type != TYPE_BREAK) {
-			var x = $("<tr><td>Duration (min):</td><td><div></div></td></tr>");
-			$("div", x).append(this.doms.lenInput);
-			dom.append(x);
-			var x = $("<tr><td>Buffer/cleanup time (min):</td><td><div></div></td></tr>");
-			$("div", x).append(this.doms.bufInput);
-			dom.append(x);
-			var x = $("<tr><td># Simultaneous teams:</td><td><div></div></td></tr>");
-			if (this.type == TYPE_JUDGING) x = $("<tr hidden><td># Simultaneous teams:</td><td><div></div></td></tr>");
-			$("div", x).append(this.doms.simInput);
-			dom.append(x);
-		}
-		if (this.type == TYPE_JUDGING)
-			var x = $("<tr><td># judging panels:</td><td><div></div></td></tr>");
-		else if (this.type == TYPE_ROUND)
-			var x = $("<tr><td># tables:</td><td><div></div></td></tr>");
-		else if (this.type == TYPE_BREAK)
-			var x = null;
-		else 
-			var x = $("<tr><td># locations:</td><td><div></div></td></tr>");
-		// var x = $("<tr><td># locations:</td><td><div></div></td></tr>");
-		if (x) {
-			$("div", x).append(this.doms.locsInput);
-			dom.append(x);
-		}
-		dom.append($("<tr><td><button class=\"btn\" onclick=\"openLocationModal("+this.uid+")\" data-toggle=\"modal\" data-target=\"#locationModal\">Edit location names</button>\
-			</td><td><button class=\"btn\" onclick=deleteParams("+this.uid+")>Delete</button></td></tr>"));
-		// Add change listeners
-        var ins = $("input,select", dom);
-        for (var i = 0; i < ins.length; i++) {
-        	$(ins[i]).attr('onchange','getSession('+this.uid+').update();');
-		}
-     	return dom;
-	}
-	this.updateDOM = function() {
-		this.doms.startDateInput.empty();
-		this.doms.endDateInput.empty();
-		for (var i = 0; i < tournament.nDays; i++)
-			this.doms.startDateInput.append($("<option value=\""+i+"\">"+tournament.days[i]+"</option>"));
-		if (tournament.nDays <= 1) this.doms.startDateInput.hide();
-		else this.doms.startDateInput.show();
-		for (var i = 0; i < tournament.nDays; i++)
-			this.doms.endDateInput.append($("<option value=\""+i+"\">"+tournament.days[i]+"</option>"));
-		if (tournament.nDays <= 1) this.doms.endDateInput.hide();
-		else this.doms.endDateInput.show();
-
-		this.doms.title[0].value = this.name;
-		this.doms.startDateInput[0].value = minsToDate(this.start);
-		this.doms.startTimeInput[0].value = minsToTime(this.start);
-		this.doms.endDateInput[0].value = minsToDate(this.end);
-		this.doms.endTimeInput[0].value = minsToTime(this.end);
-		this.doms.lenInput[0].value = this.length;
-		this.doms.bufInput[0].value = this.buffer;
-		this.doms.locsInput[0].value = this.nLocs;
-		this.doms.simInput[0].value = this.nSims;
-	}
-	this.update = function() {
-		this.name = this.doms.title[0].value;
-		this.start = tdToMins(this.doms.startDateInput[0].value,this.doms.startTimeInput[0].value);
-		this.end = tdToMins(this.doms.endDateInput[0].value,this.doms.endTimeInput[0].value);
-		this.length = this.doms.lenInput[0].value;
-		this.buffer = this.doms.bufInput[0].value;
-		this.nLocs = this.doms.locsInput[0].value;
-		if (this.type != TYPE_JUDGING) this.nSims = this.doms.simInput[0].value;
-		else this.nSims = this.nLocs;
-
-		while (this.locations.length < this.nLocs) {
-			if (this.type == TYPE_JUDGING)
-				this.locations.push("Room "+ (this.locations.length+1));
-			else if (this.type == TYPE_ROUND)
-				this.locations.push("Table "+ (this.locations.length+1));				
-			else this.locations.push("Lunch area");
-		}
-		while (this.locations.length > this.nLocs) {
-			this.locations.splice(this.locations.length-1,1);
-		}
-	}
-	this.updateDOM();
-	this.update();
-}
-
-function DomCollection() {
-	this.title=$("<input class=\"form-control\" type=text value=\"Title\">");
-	this.startDateInput=$("<select class=\"form-control\"></select>");
-	for (var i = 0; i < tournament.days.length; i++)
-		this.startDateInput.append($("<option value=\""+i+"\">"+tournament.days[i]+"</option>"));
-	if (tournament.days.length <= 1) this.startDateInput.hide();
-	else this.startDateInput.show();
-	this.startTimeInput=$("<input class=\"form-control\" type=time value=\"09:00\" step=\"900\">");
-	this.endDateInput=$("<select class=\"form-control\"></select>");
-	for (var i = 0; i < tournament.days.length; i++)
-		this.endDateInput.append($("<option value=\""+i+"\">"+tournament.days[i]+"</option>"));
-	if (tournament.days.length <= 1) this.endDateInput.hide();
-	else this.endDateInput.show();
-	this.endTimeInput=$("<input class=\"form-control\" type=time value=\"14:00\" step=\"900\">");
-	this.lenInput=$("<input class=\"form-control\" type=number min=0 max=1000 value=10>")
-	this.bufInput=$("<input class=\"form-control\" type=number min=0 max=1000 value=10>")
-	this.simInput=$("<input class=\"form-control\" type=number min=1 max=100 value=1>");
-	this.locsInput=$("<input class=\"form-control\" type=number min=1 max=100 value=1>");
 }
 
 function loadPresetFLL() {
@@ -227,27 +81,6 @@ function loadPresetFLL() {
 	addBreak("Lunch");
 }
 
-function addJudging(name,start,end,nSims,nLocs,length,buffer,locs) {
-	// alert("Hello");
-	var p = new SessionParameters(TYPE_JUDGING,name,start,end,nSims,nLocs,length,buffer,locs);
-	tournament.allSessions.push(p);
-	p.ToForm().insertBefore("#addJudgeBtn")
-}
-
-function addRound(name,start,end,nSims,nLocs,length,buffer,locs) {
-	// alert("Hello");
-	var p = new SessionParameters(TYPE_ROUND,name,start,end,nSims,nLocs,length,buffer,locs);
-	tournament.allSessions.push(p);
-	p.ToForm().insertBefore("#addRoundBtn")
-}
-
-function addBreak(name,start,end,locs) {
-	// alert("Hello");
-	var p = new SessionParameters(TYPE_BREAK,name,start,end,null,null,null,null,locs);
-	tournament.allSessions.push(p);
-	p.ToForm().insertBefore("#addBreakBtn")
-}
-
 function updateParams(id) {
 	getSession(id).update();
 }
@@ -255,9 +88,9 @@ function updateParams(id) {
 function deleteParams(id) {
 	var toDelete = -1;
 
-	for (var i = 0; i < tournament.allSessions.length; i++) {
-		if (tournament.allSessions[i].uid == id) {
-			$(tournament.allSessions[i].docObj).remove();
+	for (var i = 0; i < tourn_ui.allPanels.length; i++) {
+		if (tourn_ui.allPanels[i].session.uid == id) {
+			$(tourn_ui.allPanels[i].docObj).remove();
 			toDelete = i;
 			break;
 		}
@@ -267,25 +100,9 @@ function deleteParams(id) {
 		return;
 	}
 	tournament.allSessions.splice(toDelete,1);
+	tourn_ui.allPanels.splice(toDelete,1);
 }
 
-function minsToDate(x) {
-	return Math.floor(x/(24*60));
-}
-
-function minsToTime(x) {
-	x = (x%(24*60)) + start_time_offset;
-	var h = (x/60);
-	var m = (x%60);
-	var zh = (h < 10) ? "0" : "";
-	var zm = (m < 10) ? "0" : "";
-	return zh+h+":"+zm+m;
-}
-
-function tdToMins(d,t) {
-    var res = t.split(":");
-    return d*(60*24) + parseInt(res[0])*60 + parseInt(res[1]) - start_time_offset;
-}
 
 function getSession(uid) {
 	for (var i = 0; i < tournament.allSessions.length; i++) {
@@ -295,81 +112,5 @@ function getSession(uid) {
 	return null;
 }
 
-function copyToAll(uid) {
-	var baseSession = getSession(uid);
-	for (var i = 0; i < tournament.allSessions.length; i++) {
-		var session = tournament.allSessions[i];
-		if (session.uid != uid && session.type == baseSession.type) {
-			session.start = baseSession.start;
-			session.end = baseSession.end;
-			session.length = baseSession.length;
-			session.buffer = baseSession.buffer;
-			session.nLocs = baseSession.nLocs;
-			session.nSims = baseSession.nSims;
-			session.updateDOM();
-			session.update();
-		}
-	}
-}
 
-function openLocationModal(uid) {
-	session = getSession(uid);
-    $("#loc-modal-body").empty();
-    $("#loc-modal-body").append($("<input type=\"hidden\" value=\""+uid+"\">"));
-    for (var i = 0; i < session.locations.length; i++) {
-    	var input = $("<input type=\"text\" class=\"form-control\" value=\""+session.locations[i]+"\">");
-        $("#loc-modal-body").append(input);
-        $("#loc-modal-body").append(document.createElement("BR"));
-    }
-}
 
-function closeLocationModal() {
-	var inputs = $("#loc-modal-body>input");
-	uid = inputs[0].value;
-	session = getSession(uid);
-	for (var i = 1; i < inputs.length; i++)
-		session.locations[i-1] = inputs[i].value;
-}
-
-function openDayModal() {
-    $("#day-modal-body").empty();
-    for (var i = 0; i < tournament.nDays; i++) {
-    	var input = $("<input type=\"text\" class=\"form-control\" value=\""+tournament.days[i]+"\">");
-        $("#day-modal-body").append(input);
-        $("#day-modal-body").append(document.createElement("BR"));
-    }
-}
-
-function closeDayModal() {
-	var inputs = $("#day-modal-body>input");
-	for (var i = 0; i < inputs.length; i++)
-		tournament.days[i] = inputs[i].value;
-	updateDays();
-}
-
-function openTeamModal() {
-    $("#team-modal-body").empty();
-    $("#team-modal-body").append($("<p>One line per team.  Team numbers will automatically add\/delete to match the number of team names.</p>"))
-    var x = $("<textarea rows=\""+tournament.nTeams+"\" cols=\"5\"></textarea>");
-    for (var i = 0; i < tournament.teamNumbers.length; i++)
-    	x.append(tournament.teamNumbers[i]+"\n");
-    $("#team-modal-body").append(x);
-    var x = $("<textarea rows=\""+tournament.nTeams+"\" cols=\"60\"></textarea>");
-    for (var i = 0; i < tournament.teamNames.length; i++)
-    	x.append(tournament.teamNames[i]+"\n");
-    $("#team-modal-body").append(x);
-}
-
-function closeTeamModal() {
-	var inputs = $("#team-modal-body>textarea");
-	var nums = inputs[0].value.split("\n");
-	if (nums[nums.length-1] == "") nums.splice(nums.length-1,1);
-	var names = inputs[1].value.split("\n");
-	if (names[names.length-1] == "") names.splice(names.length-1,1);
-	while (nums.length < names.length) nums.push(""+(nums.length+1));
-	while (nums.length > names.length) nums.splice(nums.length-1,1);
-	tournament.teamNumbers = nums;
-	tournament.teamNames = names;
-	tournament.nTeams = names.length;
-	$("#nTeams")[0].value = tournament.nTeams;
-}
---
+++ scheduler/event_ui.js
@@ -0,0 +1,294 @@
+
+
+function EventPanel(params) {
+	this.params = params;
+	this.allPanels = [];
+
+	this.changeTitle = function() {
+	    var safe = this.params.name;
+	    this.params.name = prompt("Enter title here", $("#title").get(0).textContent);
+	    if (this.params.name == null) this.params.name = safe;
+	    document.getElementById("title").innerHTML = this.params.name;
+	}
+	this.changeNTeams = function() {
+		this.params.nTeams = $("#nTeams")[0].value;
+		while (this.params.teamNumbers.length < this.params.nTeams) 
+			this.params.teamNumbers.push("" + (this.params.teamNumbers.length+1)); 
+		while (this.params.teamNames.length < this.params.nTeams) 
+			this.params.teamNames.push("Team " + (this.params.teamNames.length+1)); 
+		while (this.params.teamNumbers.length > this.params.nTeams) 
+			this.params.teamNumbers.splice(this.params.teamNumbers.length,1); 
+		while (this.params.teamNames.length > this.params.nTeams) 
+			this.params.teamNames.splice(this.params.teamNames.length,1); 
+	}
+}
+
+function getPanel(uid) {
+	for (var i = 0; i < tourn_ui.allPanels.length; i++) {
+		if (tourn_ui.allPanels[i].session.uid == uid) return tourn_ui.allPanels[i];
+	}
+	console.log("Failed to find Panel " + uid);
+	return null;
+}
+
+
+function updateDays() {
+	tournament.updateDays($("#nDays")[0].value);
+	var toDelete = [];
+	for (var i = 0; i < tourn_ui.allPanels.length; i++) {
+		var panel = tourn_ui.allPanels[i];
+		if (panel.session.type == TYPE_BREAK && panel.session.end > tournament.nDays*(24*60))
+			toDelete.push(panel.session.uid);
+		while (panel.session.start > tournament.nDays*(24*60)) panel.session.start -= (24*60);
+		while (panel.session.end > tournament.nDays*(24*60)) panel.session.end -= (24*60);
+		panel.updateDOM();
+	}
+	for (var i = 0; i < toDelete.length; i++) {
+		deleteParams(toDelete[i]);
+	}
+}
+
+function sessionPanel(session) {
+	this.session = session;
+
+	// Create elements of DOM input form
+	this.docObj = $("<table class=roundtable>");
+
+	// DOM objects
+	this.title=$("<input class=\"form-control\" type=text value=\"Title\">");
+	this.startDateInput=$("<select class=\"form-control\"></select>");
+	for (var i = 0; i < tournament.days.length; i++)
+		this.startDateInput.append($("<option value=\""+i+"\">"+tournament.days[i]+"</option>"));
+	if (tournament.days.length <= 1) this.startDateInput.hide();
+	else this.startDateInput.show();
+	this.startTimeInput=$("<input class=\"form-control\" type=time value=\"09:00\" step=\"900\">");
+	this.endDateInput=$("<select class=\"form-control\"></select>");
+	for (var i = 0; i < tournament.days.length; i++)
+		this.endDateInput.append($("<option value=\""+i+"\">"+tournament.days[i]+"</option>"));
+	if (tournament.days.length <= 1) this.endDateInput.hide();
+	else this.endDateInput.show();
+	this.endTimeInput=$("<input class=\"form-control\" type=time value=\"14:00\" step=\"900\">");
+	this.lenInput=$("<input class=\"form-control\" type=number min=0 max=1000 value=10>")
+	this.bufInput=$("<input class=\"form-control\" type=number min=0 max=1000 value=10>")
+	this.simInput=$("<input class=\"form-control\" type=number min=1 max=100 value=1>");
+	this.locsInput=$("<input class=\"form-control\" type=number min=1 max=100 value=1>");
+
+	// Build docObj
+	if (this.session.type != TYPE_BREAK)
+		var x = $("<tr><td><h3></h3></td><td><button class=\"btn\" onclick=\"copyToAll("+this.session.uid+")\">Copy to all</button></td></tr>");
+	else var x = $("<tr><td colspan=\"2\"><h3></h3></td></tr>");
+	$("h3", x).append(this.title);
+	this.docObj.append(x);
+	var x = $("<tr><td>Start time:</td><td><div></div></td></tr>");
+	$("div", x).append(this.startDateInput);
+	$("div", x).append(this.startTimeInput);
+	this.docObj.append(x);
+	var x = $("<tr><td>Must be done by:</td><td><div></div></td></tr>");
+	$("div", x).append(this.endDateInput);
+	$("div", x).append(this.endTimeInput);
+	this.docObj.append(x);
+	if (this.session.type != TYPE_BREAK) {
+		var x = $("<tr><td>Duration (min):</td><td><div></div></td></tr>");
+		$("div", x).append(this.lenInput);
+		this.docObj.append(x);
+		var x = $("<tr><td>Buffer/cleanup time (min):</td><td><div></div></td></tr>");
+		$("div", x).append(this.bufInput);
+		this.docObj.append(x);
+		var x = $("<tr><td># Simultaneous teams:</td><td><div></div></td></tr>");
+		if (this.session.type == TYPE_JUDGING) x = $("<tr hidden><td># Simultaneous teams:</td><td><div></div></td></tr>");
+		$("div", x).append(this.simInput);
+		this.docObj.append(x);
+	}
+	if (this.session.type == TYPE_JUDGING)
+		var x = $("<tr><td># judging panels:</td><td><div></div></td></tr>");
+	else if (this.session.type == TYPE_ROUND)
+		var x = $("<tr><td># tables:</td><td><div></div></td></tr>");
+	else if (this.session.type == TYPE_BREAK)
+		var x = null;
+	else 
+		var x = $("<tr><td># locations:</td><td><div></div></td></tr>");
+	// var x = $("<tr><td># locations:</td><td><div></div></td></tr>");
+	if (x) {
+		$("div", x).append(this.locsInput);
+		this.docObj.append(x);
+	}
+	this.docObj.append($("<tr><td><button class=\"btn\" onclick=\"openLocationModal("+this.uid+")\" data-toggle=\"modal\" data-target=\"#locationModal\">Edit location names</button>\
+		</td><td><button class=\"btn\" onclick=deleteParams("+this.session.uid+")>Delete</button></td></tr>"));
+	// Add change listeners
+    var ins = $("input,select", this.docObj);
+    for (var i = 0; i < ins.length; i++) {
+    	$(ins[i]).attr('onchange','getPanel('+this.session.uid+').update();');
+	}
+
+	this.updateDOM = function() {
+		this.startDateInput.empty();
+		this.endDateInput.empty();
+		for (var i = 0; i < tournament.nDays; i++)
+			this.startDateInput.append($("<option value=\""+i+"\">"+tournament.days[i]+"</option>"));
+		if (tournament.nDays <= 1) this.startDateInput.hide();
+		else this.startDateInput.show();
+		for (var i = 0; i < tournament.nDays; i++)
+			this.endDateInput.append($("<option value=\""+i+"\">"+tournament.days[i]+"</option>"));
+		if (tournament.nDays <= 1) this.endDateInput.hide();
+		else this.endDateInput.show();
+
+		this.title[0].value = this.session.name;
+		this.startDateInput[0].value = minsToDate(this.session.start);
+		this.startTimeInput[0].value = minsToTime(this.session.start);
+		this.endDateInput[0].value = minsToDate(this.session.end);
+		this.endTimeInput[0].value = minsToTime(this.session.end);
+		this.lenInput[0].value = this.session.length;
+		this.bufInput[0].value = this.session.buffer;
+		this.locsInput[0].value = this.session.nLocs;
+		this.simInput[0].value = this.session.nSims;
+	}
+	this.update = function() {
+		this.session.name = this.title[0].value;
+		this.session.start = tdToMins(this.startDateInput[0].value,this.startTimeInput[0].value);
+		this.session.end = tdToMins(this.endDateInput[0].value,this.endTimeInput[0].value);
+		this.session.length = this.lenInput[0].value;
+		this.session.buffer = this.bufInput[0].value;
+		this.session.nLocs = this.locsInput[0].value;
+		if (this.session.type != TYPE_JUDGING) this.session.nSims = this.simInput[0].value;
+		else this.session.nSims = this.session.nLocs;
+
+		while (this.session.locations.length < this.session.nLocs) {
+			if (this.session.type == TYPE_JUDGING)
+				this.session.locations.push("Room "+ (this.session.locations.length+1));
+			else if (this.session.type == TYPE_ROUND)
+				this.session.locations.push("Table "+ (this.session.locations.length+1));				
+			else this.session.locations.push("Lunch area");
+		}
+		while (this.session.locations.length > this.session.nLocs) {
+			this.session.locations.splice(this.session.locations.length-1,1);
+		}
+	}
+	this.updateDOM();
+	this.update();
+
+}
+
+function copyToAll(uid) {
+	var basePanel = getPanel(uid);
+	for (var i = 0; i < tourn_ui.allPanels.length; i++) {
+		var panel = tourn_ui.allPanels[i];
+		if (panel.session.uid != uid && panel.session.type == basePanel.session.type) {
+			panel.session.start = basePanel.session.start;
+			panel.session.end = basePanel.session.end;
+			panel.session.length = basePanel.session.length;
+			panel.session.buffer = basePanel.session.buffer;
+			panel.session.nLocs = basePanel.session.nLocs;
+			panel.session.nSims = basePanel.session.nSims;
+			panel.updateDOM();
+			panel.update();
+		}
+	}
+}
+
+function addJudging(name,start,end,nSims,nLocs,length,buffer,locs) {
+	// alert("Hello");
+	var s = new SessionParameters(TYPE_JUDGING,name,start,end,nSims,nLocs,length,buffer,locs);
+	tournament.allSessions.push(s);
+	var p = new sessionPanel(s);
+	tourn_ui.allPanels.push(p);
+	p.docObj.insertBefore("#addJudgeBtn")
+}
+
+function addRound(name,start,end,nSims,nLocs,length,buffer,locs) {
+	// alert("Hello");
+	var s = new SessionParameters(TYPE_ROUND,name,start,end,nSims,nLocs,length,buffer,locs);
+	tournament.allSessions.push(s);
+	var p = new sessionPanel(s);
+	tourn_ui.allPanels.push(p);
+	p.docObj.insertBefore("#addRoundBtn")
+}
+
+function addBreak(name,start,end,locs) {
+	// alert("Hello");
+	var s = new SessionParameters(TYPE_BREAK,name,start,end,null,null,null,null,locs);
+	tournament.allSessions.push(s);
+	var p = new sessionPanel(s);
+	tourn_ui.allPanels.push(p);
+	p.docObj.insertBefore("#addBreakBtn")
+}
+
+function minsToDate(x) {
+	return Math.floor(x/(24*60));
+}
+
+function minsToTime(x) {
+	x = (x%(24*60)) + start_time_offset;
+	var h = (x/60);
+	var m = (x%60);
+	var zh = (h < 10) ? "0" : "";
+	var zm = (m < 10) ? "0" : "";
+	return zh+h+":"+zm+m;
+}
+
+function tdToMins(d,t) {
+    var res = t.split(":");
+    return d*(60*24) + parseInt(res[0])*60 + parseInt(res[1]) - start_time_offset;
+}
+
+function openLocationModal(uid) {
+	panel = getPanel(uid);
+    $("#loc-modal-body").empty();
+    $("#loc-modal-body").append($("<input type=\"hidden\" value=\""+uid+"\">"));
+    for (var i = 0; i < panel.session.locations.length; i++) {
+    	var input = $("<input type=\"text\" class=\"form-control\" value=\""+panel.session.locations[i]+"\">");
+        $("#loc-modal-body").append(input);
+        $("#loc-modal-body").append(document.createElement("BR"));
+    }
+}
+
+function closeLocationModal() {
+	var inputs = $("#loc-modal-body>input");
+	uid = inputs[0].value;
+	panel = getPanel(uid);
+	for (var i = 1; i < inputs.length; i++)
+		panel.session.locations[i-1] = inputs[i].value;
+}
+
+function openDayModal() {
+    $("#day-modal-body").empty();
+    for (var i = 0; i < tournament.nDays; i++) {
+    	var input = $("<input type=\"text\" class=\"form-control\" value=\""+tournament.days[i]+"\">");
+        $("#day-modal-body").append(input);
+        $("#day-modal-body").append(document.createElement("BR"));
+    }
+}
+
+function closeDayModal() {
+	var inputs = $("#day-modal-body>input");
+	for (var i = 0; i < inputs.length; i++)
+		tournament.days[i] = inputs[i].value;
+	updateDays();
+}
+
+function openTeamModal() {
+    $("#team-modal-body").empty();
+    $("#team-modal-body").append($("<p>One line per team.  Team numbers will automatically add\/delete to match the number of team names.</p>"))
+    var x = $("<textarea rows=\""+tournament.nTeams+"\" cols=\"5\"></textarea>");
+    for (var i = 0; i < tournament.teamNumbers.length; i++)
+    	x.append(tournament.teamNumbers[i]+"\n");
+    $("#team-modal-body").append(x);
+    var x = $("<textarea rows=\""+tournament.nTeams+"\" cols=\"60\"></textarea>");
+    for (var i = 0; i < tournament.teamNames.length; i++)
+    	x.append(tournament.teamNames[i]+"\n");
+    $("#team-modal-body").append(x);
+}
+
+function closeTeamModal() {
+	var inputs = $("#team-modal-body>textarea");
+	var nums = inputs[0].value.split("\n");
+	if (nums[nums.length-1] == "") nums.splice(nums.length-1,1);
+	var names = inputs[1].value.split("\n");
+	if (names[names.length-1] == "") names.splice(names.length-1,1);
+	while (nums.length < names.length) nums.push(""+(nums.length+1));
+	while (nums.length > names.length) nums.splice(nums.length-1,1);
+	tournament.teamNumbers = nums;
+	tournament.teamNames = names;
+	tournament.nTeams = names.length;
+	$("#nTeams")[0].value = tournament.nTeams;
+}
+
